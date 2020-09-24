c35fbf1b409d31ffdf679b6303d49d915ff0a2c1
Ryan Fowler <ryan.fowler@singlewire.com>
2020-09-24 08:57:42 -0600

    initial commit

---
+++ windows/rnwtoastactivation/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.ReactNative;
+using System.Diagnostics;
+using System.Threading;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace rnwtoastactivation
+{
+    sealed partial class App : ReactApplication
+    {
+        //private bool hasLaunched = false;
+        public App()
+        {
+#if BUNDLE
+            JavaScriptBundleFile = "index.windows";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = false;
+#else
+            JavaScriptMainModuleName = "index";
+            InstanceSettings.UseWebDebugger = true;
+            InstanceSettings.UseFastRefresh = true;
+#endif
+
+#if DEBUG
+            InstanceSettings.UseDeveloperSupport = true;
+#else
+            InstanceSettings.UseDeveloperSupport = false;
+#endif
+
+            Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
+
+            PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider());
+            PackageProviders.Add(new ReactPackageProvider());
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            base.OnLaunched(e);
+            var frame = Window.Current.Content as Frame;
+            frame.Navigate(typeof(MainPage));
+            Window.Current.Activate();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+            var frame = Window.Current.Content as Frame;
+            if (frame.GetNavigationState().Equals("1,0"))
+            {
+                frame.Navigate(typeof(MainPage));
+                Window.Current.Activate();
+            }
+        }
+    }
+}
---
+++ windows/rnwtoastactivation/Toaster.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.ReactNative.Managed;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace rnwtoastactivation
+{
+    [ReactModule("Toaster")]
+    class Toaster
+    {
+        [ReactMethod("makeToast")]
+        public void MakeToast(ReactPromise<bool> promise)
+        {
+            Debug.WriteLine("Making Toast");
+            ShowToast("Id", "Some toast", "mmm, toasty!");
+            promise.Resolve(true);
+        }
+        private static void ShowToast(string id, string subject, string body)
+        {
+            ToastContent toastContent = MakeContent(id, subject, body);
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml())
+            {
+                Tag = "foo"
+            };
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+        static ToastContent MakeContent(string id, string subject, string body)
+        {
+            string title = subject;
+
+            // Construct the visuals of the toast
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = title
+                        },
+                        new AdaptiveText()
+                        {
+                            Text = body
+                        },
+                    },
+                    AppLogoOverride = new ToastGenericAppLogo()
+                    {
+                        Source = "ms-appdata:///local/Andrew.jpg",
+                        HintCrop = ToastGenericAppLogoCrop.Circle
+                    }
+                }
+            };
+            var qs = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            qs.Add("subject", subject);
+            qs.Add("body", body);
+            qs.Add("id", id);
+            // Now we can construct the final toast content
+            ToastContent toastContent = new ToastContent()
+            {
+                Launch = qs.ToString(),
+                Visual = visual,
+                Scenario = ToastScenario.Alarm,
+                Actions = new ToastActionsCustom() { Buttons = { new ToastButton("Click Here!", qs.ToString()) } }
+
+
+                // Arguments when the user taps body of toast
+            };
+            return toastContent;
+        }
+    }
+}
